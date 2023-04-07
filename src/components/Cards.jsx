2b3f8ecefc73d4e9a6c8599be1827af0bf9a5506
iamakeem <bolarinwa_akeem@yahoo.com>
2023-04-07 20:02:57 +0100

    added card data

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,12 @@ function Card (props) {
   return (
     <div className='card-style'>
       <div className='card-content'>
-        <img alt='card' src={props.images} className='card-design' />
+        <img
+          alt='card'
+          // src={props.images}
+          src={`../images/${props.img}`}
+          className='card-design'
+        />
         <p>
           <img alt='star' src='images/Star1.png' className='star-style' />
           5.0 (6).USA
@@ -15,7 +20,6 @@ function Card (props) {
           <span>From $136 </span>/ person
         </p>
       </div>
-      
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
+// `@chakra-ui/theme` is a part of the base install with `@chakra-ui/react`
+import chakraTheme from '@chakra-ui/theme';
+
+const { Button } = chakraTheme.components;
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraBaseProvider theme={theme}>
+      <App />
+    </ChakraBaseProvider>
+  </React.StrictMode>
+);
